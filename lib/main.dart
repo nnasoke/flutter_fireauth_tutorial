01dd59b7939a72e6ca284d5406dc20ead434a97f
nnasoke <naruemit.nasoke@gmail.com>
2022-07-14 22:55:02 +0700

    Add logged screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,14 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_fireauth_tutorial/app_router.dart';
 
-void main() {
+import 'firebase_options.dart';
+
+void main() async {
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(const MyApp());
 }
 
@@ -10,7 +17,11 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
+      theme: ThemeData(
+        primarySwatch: Colors.blueGrey,
+        fontFamily: 'Prompt',
+      ),
       onGenerateRoute: AppRouter.generateRoute,
       initialRoute: AppRouter.home,
     );
---
+++ lib/screens/user_logged.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class UserLoggedScreen extends StatefulWidget {
+  UserLoggedScreen({Key? key}) : super(key: key);
+
+  @override
+  State<UserLoggedScreen> createState() => _UserLoggedScreenState();
+}
+
+class _UserLoggedScreenState extends State<UserLoggedScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
